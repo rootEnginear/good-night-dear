d3e518731e102121e9e782427de069c08daee81e
rootEnginear <suthep.chanchuphol@outlook.com>
2022-03-09 20:05:50 +0700

    feat: formatting files

--- app.js
+++ app.js
@@ -1,80 +1,76 @@
 var app = new Vue({
-  el: "#app",
-  mounted: function() {
-    fetch("./assets/story.txt").then(
-      res => res.text()
-    ).then(text => {
-      this.data = text.split(" ");
-      this.dataLength = this.data.length;
-      this.isLoading = false;
-      this.generateStory();
-    });
-  },
-  data: function() {
-    return {
-      skoy: new Skoy(),
-      isLoading: true,
-      // Data
-      data: "",
-      dataLength: 0,
-      // Story
-      storyLength: 5,
-      currentStory: "กำลังโหลด...",
-      userTranslation: "",
-      // Modal
-      modalContentCorrent: "",
-      modalContentInput: "",
-      modalPercentage: 0.0
-    };
-  },
-  computed: {
-    skoyStory: function() {
-      return this.skoy.convert(this.currentStory);
-    }
-  },
-  methods: {
-    toggleModal: function(ref) {
-      this.$refs[ref].click();
-    },
-    generateStory: function() {
-      var startingIndex = Math.floor(
-        Math.random() * (this.dataLength - this.storyLength)
-      );
-      this.currentStory = this.data
-        .slice(startingIndex, startingIndex + this.storyLength)
-        .join(" ");
-      this.userTranslation = "";
-    },
-    checkStory: function() {
-      var storyDifferences = Diff.diffChars(
-          this.currentStory,
-          this.userTranslation.trim().replace(/\s+/g, " ")
-        ),
-        diffReal = "",
-        diffInput = "",
-        charLength = 0,
-        charCorrect = 0;
-      storyDifferences.forEach(function(part) {
-        if (part.added) {
-          diffInput += "<span style='color:red'>" + part.value + "</span>";
-        } else if (part.removed) {
-          charLength += part.value.length;
-          diffReal += "<span style='color:green'>" + part.value + "</span>";
-        } else {
-          diffInput += part.value;
-          diffReal += part.value;
-          charCorrect += part.value.length;
-          charLength += part.value.length;
-        }
-      });
-      this.modalContentCorrent = diffReal;
-      this.modalContentInput = diffInput;
-      this.modalPercentage = Math.round((charCorrect / charLength) * 1e4) / 1e2;
-      this.toggleModal("solutionModal");
-    },
-    newStory: function() {
-      this.generateStory();
-      this.toggleModal("solutionModal");
-    }
-  }
-});
+	el: '#app',
+	mounted: function () {
+		fetch('./assets/story.txt')
+			.then((res) => res.text())
+			.then((text) => {
+				this.data = text.split(' ')
+				this.dataLength = this.data.length
+				this.isLoading = false
+				this.generateStory()
+			})
+	},
+	data: function () {
+		return {
+			skoy: new Skoy(),
+			isLoading: true,
+			// Data
+			data: '',
+			dataLength: 0,
+			// Story
+			storyLength: 5,
+			currentStory: 'กำลังโหลด...',
+			userTranslation: '',
+			// Modal
+			modalContentCorrent: '',
+			modalContentInput: '',
+			modalPercentage: 0.0,
+		}
+	},
+	computed: {
+		skoyStory: function () {
+			return this.skoy.convert(this.currentStory)
+		},
+	},
+	methods: {
+		toggleModal: function (ref) {
+			this.$refs[ref].click()
+		},
+		generateStory: function () {
+			var startingIndex = Math.floor(Math.random() * (this.dataLength - this.storyLength))
+			this.currentStory = this.data.slice(startingIndex, startingIndex + this.storyLength).join(' ')
+			this.userTranslation = ''
+		},
+		checkStory: function () {
+			var storyDifferences = Diff.diffChars(
+					this.currentStory,
+					this.userTranslation.trim().replace(/\s+/g, ' ')
+				),
+				diffReal = '',
+				diffInput = '',
+				charLength = 0,
+				charCorrect = 0
+			storyDifferences.forEach(function (part) {
+				if (part.added) {
+					diffInput += "<span style='color:red'>" + part.value + '</span>'
+				} else if (part.removed) {
+					charLength += part.value.length
+					diffReal += "<span style='color:green'>" + part.value + '</span>'
+				} else {
+					diffInput += part.value
+					diffReal += part.value
+					charCorrect += part.value.length
+					charLength += part.value.length
+				}
+			})
+			this.modalContentCorrent = diffReal
+			this.modalContentInput = diffInput
+			this.modalPercentage = Math.round((charCorrect / charLength) * 1e4) / 1e2
+			this.toggleModal('solutionModal')
+		},
+		newStory: function () {
+			this.generateStory()
+			this.toggleModal('solutionModal')
+		},
+	},
+})
